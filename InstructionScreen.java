85e2b4cfc92ce349c4cb952beb0c65dc97ad038a
jamie wang <bombyjamie@gmail.com>
2024-10-21 22:27:35 -0400

    created instruction screen code

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -1,11 +1,37 @@
 import greenfoot.*;
 
 public class InstructionScreen extends World {
-    public InstructionScreen() {
-        super(600, 400, 1);
+    private MenuScreen menuScreen;
+    private String[] instructions = {   "Instruction 1", "Instruction Screen 2" };
 
-    addObject(new Button(this::goInstructions), 300, 340);
-  }
+    private int currentIndex = 0;
+    private Label instructionLabel;
+    public InstructionScreen(MenuScreen menuScreen) 
+    {
+        super(600, 400, 1);
+        this.menuScreen = menuScreen;
+        instructionLabel = new Label(instructions[currentIndex], 24);
+        addObject(instructionLabel, 300, 200); 
+        addObject(new Button(this::cycleInstruction), 300, 300);
+        addObject(new Button(this::goBack), 300, 350);
+    }
+    public void cycleInstruction() 
+    {
+        currentIndex++;
+        if (currentIndex >= instructions.length) 
+        {
+            currentIndex = 0;
+        }
+        removeObject(instructionLabel);
+        instructionLabel = new Label(instructions[currentIndex], 24);
+        addObject(instructionLabel, 300, 200);
+    }
+
+    public void returnMenu() 
+    {
+        Greenfoot.setWorld(menuScreen);
+    }
+}
 
   
 
